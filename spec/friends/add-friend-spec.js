76179fa68132ef40542c3cc18a71108fb461baac
Nicole Griesmeyer <nicole@griesmeyer.org>
2020-03-27 09:06:56 -0700

    put call to server listen outside of test context

--- spec/friends/add-friend-spec.js
+++ spec/friends/add-friend-spec.js
@@ -3,17 +3,19 @@ import server from "../../source/app";
 
 let app;
 
+beforeAll((done) => {
+  app = server.listen(process.env.PORT, done);
+});
+
 describe("addFriend", () => {
-  beforeAll((done) => {
-    app = server.listen(process.env.PORT, done);
-  });
 
   it("responds with a 200 status", async () => {
     let response = await request(app).post(`/friends`).send({ name: "hello" });
     expect(response.status).toEqual(200);
   });
 
-  afterAll( (done) => {
-    app.close(done);
-  });
+});
+
+afterAll( (done) => {
+  app.close(done);
 });
