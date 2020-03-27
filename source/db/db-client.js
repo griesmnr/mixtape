ad9f2e9a8306d69e7017dd12766562dcbbfbfc95
Nicole Griesmeyer <nicole@griesmeyer.org>
2020-03-26 17:02:01 -0700

    add first shot at database client

---
+++ source/db/db-client.js
@@ -0,0 +1,10 @@
+const low = require('lowdb')
+const lodashId = require('lodash-id')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+db._.mixin(lodashId)
+
+export default db;
