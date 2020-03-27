b47c424cac163ce55356fda1706e525fa8142d00
Nicole Griesmeyer <nicole@griesmeyer.org>
2020-03-26 22:36:15 -0700

    add one set of jest tests

---
+++ spec/friends/add-friend-spec.js
@@ -0,0 +1,19 @@
+import request from "supertest";
+import server from "../../source/app";
+
+let app;
+
+describe("addFriend", () => {
+  beforeAll((done) => {
+    app = server.listen(process.env.PORT, done);
+  });
+
+  it("responds with a 200 status", async () => {
+    let response = await request(app).post(`/friends`).send({ name: "hello" });
+    expect(response.status).toEqual(200);
+  });
+
+  afterAll( (done) => {
+    app.close(done);
+  });
+});
